a2a0028da544425d8799ede5a73b679d97bde634
Henry Atudume <atuhen14@gmail.com>
2025-06-09 03:16:15 +0100

    More Notes added

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+let age = 25;
+let firstName = "Olawale";
+let lastName = "Wellington"
+
+if (age<20){
+    console.log('not eligible to vote')
+   
+} else{
+    console.log('Eligible')
+    //Learning methods
+     console.log('my name is', firstName.toUpperCase())
+}
+console.log(age, firstName)
+
+// Arrays
+
+let fruits = ["Mango","Banana", "orange"]
+
+fruits[1] = "Shawashop"
+
+console.log(fruits)
+
+let firstInitial = firstName.charAt()
+let secInitial = lastName.charAt()
+
+console.log(firstInitial + secInitial)
+
+
+
---
+++ read.md
@@ -0,0 +1,17 @@
+Type          Examples
+Sting         "Hello"
+Number        42,3,14
+Bolean        True, False
+Null          Null
+Undefined     Undefined
+Object        {name:"Alice"}
+Array         [1,2,3]
+
+
+String Methods
+1. toUpperCase()
+2. toLowerCase()
+3. charAt()
+4. slice()
+
+Notes : strings are immutable meaning once declared, they can not be replaced
