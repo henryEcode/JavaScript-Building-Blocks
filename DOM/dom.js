adb730bed6957a1a67876964e84aba73f434207d
Henry Atudume <atuhen14@gmail.com>
2025-08-26 17:20:33 +0100

    New DOMLesson

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,10 +1,16 @@
 let btn = document.getElementById("btn");
 
 let heading  = document.getElementsByTagName("h1")[0];
+let second = document.getElementById("second-h1");
+const grad = document.querySelector(".querry");
+
 
 function changeStyle(){
     heading.style.color = "red";
+    heading.innerText = "I just change this through DOM manipulation";
+    second.classList.add("box-italics")
 }
 
-console.log(btn)
+//window.alert(`Get me there`)
+console.log(grad)
 btn.addEventListener("click", changeStyle);
