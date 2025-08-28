15bf72ca3e9ae4d57fd66d419a6c1748c7bf8612
Henry Atudume <atuhen14@gmail.com>
2025-08-28 09:28:37 +0100

    updated

--- DOM/dom.js
+++ DOM/dom.js
@@ -3,6 +3,7 @@ let btn = document.getElementById("btn");
 let heading  = document.getElementsByTagName("h1")[0];
 let second = document.getElementById("second-h1");
 const grad = document.querySelector(".querry");
+let container = document.querySelector(".container")
 
 
 function changeStyle(){
@@ -11,6 +12,22 @@ function changeStyle(){
     second.classList.add("box-italics")
 }
 
+function addElem(){
+    const addEle = document.createElement('p');
+    addEle.innerText = "We are outside";
+    container.appendChild(addEle)
+
+}
+
+ setTimeout(()=>{console.log(`this will run after 10 seconds`)},2000)
+  setInterval(()=>{console.log(`this will run after 10 seconds`)},4000)
+ 
+function remElem(){
+ 
+   document.body.removeChild(container)
+}
+
 //window.alert(`Get me there`)
-console.log(grad)
+console.log('body text:',document.body.innerText)
 btn.addEventListener("click", changeStyle);
+btn2.addEventListener('click', remElem)
